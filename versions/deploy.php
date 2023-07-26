79a0b7748d6df06494bef398c7f77845a35775ae
Josimar Camilo <josimarifmg@gmail.com>
2023-07-25 21:17:53 -0300

    melhoria no deploy

--- versions/deploy.php
+++ versions/deploy.php
@@ -1,6 +1,10 @@
 <?php
 
 /**
+ * esse arquivo deve ficar na pasta version no servidor compartilhado
+ */
+
+/**
  * fazer uma copia da pasta public_html
  * cp -r public_html public_html_back
  *
@@ -20,8 +24,8 @@
  * php artisan view:cache
  */
 
-$directory_versions = "versions";
-$file_currete_version = "releases/current/version.txt";
+$directory_versions = ".";
+$file_currete_version = "/releases/current/version.txt";
 
 $content = scandir($directory_versions);
 
@@ -30,14 +34,17 @@ if (! $content) {
     return;
 }
 
+echo("tem versao". PHP_EOL);
+
 $versions = array_filter($content, function ($value) {
     return str_contains($value, '.zip') && str_starts_with($value, '20');
 });
 
 if (count($versions) == 0) {
-    echo("never version ". PHP_EOL);
+    echo("nenhum versao encontrada ". PHP_EOL);
     return;
 }
+
 
 foreach($versions as $key => $version) {
     $versions[$key] = str_replace('.zip', "", $version);
@@ -47,59 +54,52 @@ sort($versions);
 
 $last_version = end($versions);
 
-$current_version = file_get_contents($file_currete_version);
-
-if ($current_version >= $last_version) {
-    // echo("nao fazer deploy ". PHP_EOL);
-    // echo("versao atual $current_version". PHP_EOL);
-    // echo("ultima $last_version". PHP_EOL);
-
-    return;
-}
+echo("versao encontrada $last_version". PHP_EOL);
 
 $result = '';
 $resultCode = false;
 
-try {
-    exec('cd releases/current && php artisan migrate', $result, $resultCode);
-} catch(Throwable $tr) {
-    echo "erro ao executar as migrates". PHP_EOL;
-    echo $tr->getMessage(). PHP_EOL;
-    return;
-}
-
-if ($result === false) {
-    echo "erro ao executar as migrates". PHP_EOL;
-    return;
-}
-
-$from = $directory_versions.'/'. $last_version.'.zip';
-$to = 'releases'.'/'.$last_version.'.zip';
+$from = $last_version.'.zip';
+$to = '../releases'.'/'.$last_version.'.zip';
 
 copy($from, $to);
+
+echo("arquivo copiado de $from". PHP_EOL);
+echo("para $to". PHP_EOL);
 
 $zip = new ZipArchive();
 
 $zip->open($to);
 
-$ok = $zip->extractTo('./releases');
+$ok = $zip->extractTo("../releases/$last_version");
 
 $zip->close();
 
+echo("extracao do arquivo $to concluida". PHP_EOL);
+
 unlink($to);
 
+echo("arquivo $to excluido". PHP_EOL);
+
+$pasta_current = '../releases/current';
 //symbolic
-if (is_link('releases/current')) {
-    unlink('releases/current');
+if (is_link($pasta_current)) {
+    echo("$pasta_current é um link". PHP_EOL);
+    unlink($pasta_current);
+    echo('link deletado'. PHP_EOL);
+} else {
+    echo("$pasta_current nao é link". PHP_EOL);
 }
 
-chdir('releases');
+chdir('../releases');
 
 exec("ln -s $last_version current", $result, $resultCode);
 
 if ($result === false) {
     echo "erro ao criar o link limbolico.". PHP_EOL;
     return;
+} else {
+    echo("link criado com sucesso $last_version para current ". PHP_EOL);
 }
 
-echo(PHP_EOL. "version $last_version in production". PHP_EOL);
+echo(PHP_EOL. "versao $last_version publicada". PHP_EOL);
