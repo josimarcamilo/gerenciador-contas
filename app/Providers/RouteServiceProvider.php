ae53f7ab78333aa881bf604fd7dd213b2bbaa60f
Josimar Camilo <josimarifmg@gmail.com>
2023-10-17 21:03:37 -0300

    perf: :zap: adicionando rate limiter nas rotas da web

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,5 +48,8 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+        RateLimiter::for('web', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
