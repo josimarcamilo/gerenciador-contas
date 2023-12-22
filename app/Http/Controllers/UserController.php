a5a779dbdf57703ad111f0020d6d918ec94bedef
Josimar Camilo <josimarifmg@gmail.com>
2023-12-22 16:48:31 -0300

    Criando conta padrao ao criar um usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use App\Models\User;
 use Exception;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Throwable;
 
 class UserController extends Controller
@@ -22,13 +24,17 @@ class UserController extends Controller
             if (User::where('email', $credentials['email'])->exists()) {
                 throw new Exception('User already exists');
             }
-
+            DB::beginTransaction();
             $user = User::create($credentials);
-
+            $accountDefault = new Account();
+            $accountDefault->user_id = $user->id;
+            $accountDefault->save();
+            DB::commit();
             event(new Registered($user));
 
             return response()->json(['data' => $user], 201);
         } catch (Throwable $th) {
+            DB::rollBack();
             return response()->json([
                 'message' => $th->getMessage(),
             ], 422);
